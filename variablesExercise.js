77f28cbf69c5f79b95446959b1c30cfb0c6324fd
Maleah Walden <maleah13walden@gmail.com>
2020-10-13 13:49:52 -0500

    Completed

---
+++ variablesExercise.js
@@ -0,0 +1,11 @@
+let myAge = 17;
+myAge = 12;
+myAge += 6;
+myAge--
+myAge = "None of your business"
+console.log(myAge);
+let blank;
+blank = null;
+console.log(blank);
+let notANumber = 0 * Infinity;
+console.log(notANumber);
